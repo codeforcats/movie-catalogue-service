cdcd5678f8bc63f0b539ba5a95893133e1197c3e
codeforcats <richard.spurr@gmail.com>
2022-05-25 08:49:44 +0100

    api to return hard-coded list of one movie.

---
+++ src/main/java/com/rjs/moviecatalogueservice/model/CatalogueItem.java
@@ -0,0 +1,3 @@
+package com.rjs.moviecatalogueservice.model;
+
+public record CatalogueItem(String name, String description, int rating){ }
---
+++ src/test/java/com/rjs/moviecatalogueservice/MovieCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjs.moviecatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
