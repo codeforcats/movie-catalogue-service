08ec5071f67c3fbbdd4c326392ac2a5d854b6428
codeforcats <richard.spurr@gmail.com>
2022-05-25 09:47:00 +0100

    tinkering

--- src/main/java/com/rjs/moviecatalogueservice/resource/MovieCatalogueResource.java
+++ src/main/java/com/rjs/moviecatalogueservice/resource/MovieCatalogueResource.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("catalogue")
+@RequestMapping("/catalogues")
 public class MovieCatalogueResource {
 
     @RequestMapping("/{userId}")
