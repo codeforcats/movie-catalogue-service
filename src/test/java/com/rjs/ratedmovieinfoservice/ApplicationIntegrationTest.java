02424eccaa1f61279eee3ca29798df33c2f2ed03
codeforcats <richard.spurr@gmail.com>
2022-06-02 19:15:43 +0100

    optimised imports

--- src/test/java/com/rjs/ratedmovieinfoservice/ApplicationIntegrationTest.java
+++ src/test/java/com/rjs/ratedmovieinfoservice/ApplicationIntegrationTest.java
@@ -2,21 +2,15 @@ package com.rjs.ratedmovieinfoservice;
 
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest()
 public class ApplicationIntegrationTest {
